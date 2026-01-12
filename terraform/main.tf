297fec604a74e4dc00881476918f3007b23a8f8f
Nouman Ahmad <noumansdo737@gmail.com>
2026-01-12 23:25:27 +0500

    Deploy Lambda and API Gateway using Terraform

---
+++ terraform/main.tf
@@ -0,0 +1,87 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.27.0"
+    }
+  }
+}
+
+provider "aws" {
+  # use variables or environment variables to configure the region and credentials
+    region = var.aws_region
+    profile = var.aws_profile
+}
+# IAM role for Lambda
+# IAM role for Lambda
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_basic_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# IAM policy attachment
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role      = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Zip Lambda code
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "../app"
+  output_path = "../lambda.zip"
+}
+
+# Lambda function
+resource "aws_lambda_function" "hello_lambda" {
+  function_name = "hello-world-lambda"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "handler.handler"
+  runtime       = "nodejs18.x"
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+}
+
+# API Gateway
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "hello-http-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id           = aws_apigatewayv2_api.http_api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.hello_lambda.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "default_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "GET /"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+# Allow API Gateway to call Lambda
+resource "aws_lambda_permission" "api_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hello_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
